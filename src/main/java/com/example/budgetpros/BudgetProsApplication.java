1f1cea3fd8e983e7eee584abdabb98a8954c8b42
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-08-11 15:21:13 -0500

    first commit

---
+++ src/main/java/com/example/budgetpros/BudgetProsApplication.java
@@ -0,0 +1,13 @@
+package com.example.budgetpros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetProsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetProsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/budgetpros/BudgetProsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budgetpros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetProsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
