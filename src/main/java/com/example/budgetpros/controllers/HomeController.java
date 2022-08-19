2b3d09e93ae8b63ec83f6352fcc2e2d27d5a112d
Mark Siebald <siebald.mark93@gmail.com>
2022-08-19 09:07:14 -0500

    loginpageview again

--- src/main/java/com/example/budgetpros/controllers/HomeController.java
+++ src/main/java/com/example/budgetpros/controllers/HomeController.java
@@ -1,2 +1,14 @@
-package com.example.budgetpros.controllers;public class HomeController {
+package com.example.budgetpros.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HomeController {
+
+    @GetMapping("/home")
+    public String returnHomePage() {
+        return "index";
+    }
 }
