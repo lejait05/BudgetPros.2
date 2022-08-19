d0f208d389bbf8381e9c1cb8bceed8f2b2297e75
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-08-19 14:58:08 -0400

    feat: file changes

--- src/main/java/com/example/budgetpros/controllers/HomeController.java
+++ src/main/java/com/example/budgetpros/controllers/HomeController.java
@@ -1,11 +1,8 @@
 package com.example.budgetpros.controllers;
 
-import com.example.budgetpros.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HomeController {
