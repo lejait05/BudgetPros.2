956ccec2d392ea1ed3b88d5832541ce76bf23bee
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-08-19 15:22:09 -0500

    make changes to resolve merging issues

--- src/main/java/com/example/budgetpros/repositories/TransactionRepository.java
+++ src/main/java/com/example/budgetpros/repositories/TransactionRepository.java
@@ -3,6 +3,8 @@ package com.example.budgetpros.repositories;
 import com.example.budgetpros.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+import java.util.List;
 
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+ List<Transaction> findByUserId(Long id);
 }
--- src/main/java/com/example/budgetpros/repositories/UserRepository.java
+++ src/main/java/com/example/budgetpros/repositories/UserRepository.java
@@ -4,6 +4,5 @@ import com.example.budgetpros.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
     User findByUsername(String username);
 }
